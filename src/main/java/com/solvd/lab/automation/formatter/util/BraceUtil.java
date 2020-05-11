9f7b5f42c3f965aa955e0625ace0024086374d11
DimkaKazak <Kazak211099@gmail.com>
2020-05-11 21:08:19 +0300

    add: solution

--- src/main/java/com/solvd/lab/automation/formatter/util/BraceUtil.java
+++ src/main/java/com/solvd/lab/automation/formatter/util/BraceUtil.java
@@ -1,4 +1,30 @@
 package com.solvd.lab.automation.formatter.util;
 
 public class BraceUtil {
+
+    public static String normalizeSharpBraces(String input) {
+
+        StringBuilder result = new StringBuilder();
+
+        char[] copiedCharArray = input.toCharArray();
+        for (int i = 0; i < copiedCharArray.length; i++){
+
+            if (copiedCharArray[i] == '{'){
+
+                if (copiedCharArray[i - 1] == ' '){
+                    result.append("{\n");
+                } else {
+                    result.append(" {\n");
+                }
+
+            } else if (copiedCharArray[i] == '}'){
+                result.append("\n}");
+            } else {
+                result.append(copiedCharArray[i]);
+            }
+
+        }
+
+        return String.valueOf(result);
+    }
 }
